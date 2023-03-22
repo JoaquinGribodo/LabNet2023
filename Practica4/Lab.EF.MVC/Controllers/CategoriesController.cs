49b8713005423ad09313f7b2ee646bacfbd6e6a1
Joaquin Gribodo <joacogribodo@gmail.com>
2023-03-22 10:33:59 -0300

    PracticaMVC

---
+++ Practica4/Lab.EF.MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,117 @@
+﻿using Lab.EF.MVC.Models;
+using LabEF.Entities;
+using LabEF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.EF.MVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        CategoriesLogic logic = new CategoriesLogic();
+        // GET: Categories
+        public ActionResult Index()
+        {
+            try
+            {
+                List<LabEF.Entities.Categories> categories = logic.GetAll();
+                List<CategoriesView> categoriesView = categories.Select(c => new CategoriesView
+                {
+                    Id = c.CategoryID,
+                    Nombre = c.CategoryName,
+                    Descripcion = c.Description
+                }).ToList();
+
+                return View(categoriesView);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(CategoriesView categoriesView)
+        {
+            try
+            {
+                Categories categoryEntity = new Categories
+                {
+                    CategoryID = categoriesView.Id,
+                    CategoryName = categoriesView.Nombre,
+                    Description = categoriesView.Descripcion
+                };
+
+                if (ModelState.IsValid)
+                {
+                    logic.Add(categoryEntity);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(categoriesView);
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                logic.Remove(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                var entity = logic.GetAll().Where(e => e.CategoryID == id).FirstOrDefault();
+                CategoriesView model = new CategoriesView
+                {
+                    Id = entity.CategoryID,
+                    Nombre = entity.CategoryName,
+                    Descripcion = entity.Description
+                };
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CategoriesView categoriesView)
+        {
+            try
+            {
+                logic.Update(new Categories { CategoryID = categoriesView.Id, CategoryName = categoriesView.Nombre, Description = categoriesView.Descripcion });
+                                                                              //El nombre nuevo se pasa bien, pero no se modifica en la tabla
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Error");
+            } 
+        }
+    }
+}
---
+++ Practica4/Lab.EF.MVC/Models/CategoriesView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab.EF.MVC.Models
+{
+    public class CategoriesView
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre no puede estar vacío.")]
+        [StringLength(15)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripción no puede estar vacío.")]
+        [StringLength(80)]
+        public string Descripcion { get; set; }
+
+    }
+}
