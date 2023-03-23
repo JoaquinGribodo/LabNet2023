4c8c8597b1baa68ad79678f3b5dca231609db468
Joaquin Gribodo <joacogribodo@gmail.com>
2023-03-23 09:33:11 -0300

    Modificacion

--- Practica4/Lab.EF.MVC/Models/CategoriesView.cs
+++ Practica4/Lab.EF.MVC/Models/CategoriesView.cs
@@ -11,11 +11,11 @@ namespace Lab.EF.MVC.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo Nombre no puede estar vacío.")]
-        [StringLength(15)]
+        [StringLength(15, ErrorMessage = "El campo Nombre no puede tener más de 15 caracteres.")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage = "El campo Descripción no puede estar vacío.")]
-        [StringLength(80)]
+        [StringLength(80, ErrorMessage = "El campo Descripción no puede tener más de 80 caracteres.")]
         public string Descripcion { get; set; }
 
     }
